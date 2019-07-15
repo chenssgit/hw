b7cec67ebb9274818d57adda1492f69836c0e817
chenssgit <chenshangsong@hotmail.com>
2019-07-16 07:55:08 +0800

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,10 +1,12 @@
 import numpy as np
-import scipy.special
+import math
 
 class NN:
   def __init__(self, n0, n1, n2):
     self.n0, self.n1, self.n2 = n0, n1, n2
-  def activation(self, inputs): return scipy.special.expit(inputs)
+    self.w0 = np.random.normal(0.0, n1**-0.5, (n1, n0))
+    self.w1 = np.random.normal(0.0, n2**-0.5, (n2, n1))
+  def activation(self, inputs): return 1.0/(1.0-math.e**-inputs)
   
   def train(self, inputs, targets):
     outputs2 = self.query(inputs)
@@ -16,13 +18,17 @@ class NN:
     
   def query(self, inputs):
     inputs0 = np.array(inputs, ndmin=2).T
-    inputs1 = self.w0*inputs0
+    inputs1 = np.dot(self.w0, inputs0)
     outputs1 = self.activation(inputs1)
     inputs2 = self.w1*inputs1
     outputs2 = self.activation(inputs2)
     
-with f,t=open('data.csv','r'), open('test.csv','r'):
-  lines,tests = f.read_lines(),t.read_lines()
-ai = NN(764, 100, 10)
-ai.trains(lines)
-print(ai.query(tests[0]))
+f=open('a.txt','r')
+inputs = f.readlines()
+f.close
+ai = NN(784, 100, 10)
+
+line = np.asfarray(inputs[0].split(',')[1:])#.reshape((28,28))
+#ai.query(line)
+#print(ai.query(tests[0]))
+print(np.array([1,2,3,4,5,6], ndmin=))
